090d375675796941de47c2e8f22f97abcb36f93a
河儿 <60065901+jay-pro@users.noreply.github.com>
2021-11-06 17:52:14 +0700

    update

--- ZSamples/coin_catch-master/coin_catch-master/models/palette.py
+++
@@ -1,9 +0,0 @@
-"""
-palette.py - contains color related data.
-"""
-# Basic colors
-black = (000, 000, 000)
-white = (255, 255, 255)
-red   = (255, 000, 000)
-green = (000, 255, 000)
-blue  = (000, 000, 255)
